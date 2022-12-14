9fcc550f418fefeb0127e518483deb85fdc2d2d7
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-14 15:07:38 +0100

    [add] array

---
+++ db/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$categories = [
+
+    'gatto' => new Category('Gatto', '<i class="fa-solid fa-cat"></i>'),
+    'cane' => new Category('Cane', '<i class="fa-solid fa-dog"></i>'),
+    'pesce' => new Category('Pesce', '<i class="fa-solid fa-fish"></i>'),
+    'uccello' => new Category('Uccello', '<i class="fa-solid fa-dove"></i>')
+    
+]
